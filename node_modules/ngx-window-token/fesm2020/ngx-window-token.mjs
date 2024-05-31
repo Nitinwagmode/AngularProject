acc15297249299b10c08fe2a8812ae89164cfc4c
Nitinwagmode <wagmodenitin@gmail.com>
2024-05-31 12:49:29 +0530

    angular project

---
+++ node_modules/ngx-window-token/fesm2020/ngx-window-token.mjs
@@ -0,0 +1,16 @@
+import { InjectionToken } from '@angular/core';
+
+const WINDOW = new InjectionToken('WindowToken', typeof window !== 'undefined' && window.document
+    ? { providedIn: 'root', factory: () => window }
+    : { providedIn: 'root', factory: () => undefined });
+
+/*
+ * Public API Surface of ngx-window-token
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { WINDOW };
+//# sourceMappingURL=ngx-window-token.mjs.map
